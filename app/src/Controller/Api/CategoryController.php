6b52acd891459fe6a077b454d7a5902aeacd3088
Pedro Medina Márquez <medinamarquezp@gmail.com>
2020-12-15 02:40:36 +0100

    refactor: category manager migrated

--- app/src/Controller/Api/CategoryController.php
+++ app/src/Controller/Api/CategoryController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller\Api;
 
-use App\Service\CategoryManager;
+use App\Entity\Category;
+use App\Service\ValidateService;
 use App\Exceptions\BaseException;
 use App\Exceptions\NotFoundException;
 use App\Exceptions\ValidationException;
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,15 +21,14 @@ class CategoryController extends BaseController
   /**
    * @Route("/category", methods={"POST"})
    */
-  public function create(Request $request, CategoryManager $categoryManager): JsonResponse
+  public function create(Request $request, CategoryRepository $repo, ValidateService $v): JsonResponse
   {
     try {
-      $category = $categoryManager->serialize($request);
-
-      $errors = $categoryManager->validate($category);
+      $category =  $this->deserialize($request, Category::class);
+      $errors = $v->validate($category);
       if ($errors) throw new ValidationException($errors);
 
-      $categoryCreated = $categoryManager->save($category);
+      $categoryCreated = $repo->save($category);
       return $this->success("Category created correctly", $categoryCreated);
     } catch (BaseException $ex) {
       return $this->fail($ex);
@@ -39,18 +40,18 @@ class CategoryController extends BaseController
    * requirements={"id"="\d+"},
    * methods={"PUT"})
    */
-  public function update(Request $request, CategoryManager $categoryManager): JsonResponse
+  public function update(Request $request, CategoryRepository $repo, ValidateService $v): JsonResponse
   {
     try {
-      $category = $categoryManager->find($request->get('id'));
+      $category = $repo->find($request->get('id'));
       if (!$category) throw new NotFoundException("Category not found");
 
-      $requestData =  $categoryManager->serialize($request);
+      $requestData =  $this->deserialize($request, Category::class);
 
-      $errors = $categoryManager->validate($requestData);
+      $errors = $v->validate($requestData);
       if ($errors) throw new ValidationException($errors);
 
-      $categoryUpdated = $categoryManager->update($category, $requestData);
+      $categoryUpdated = $repo->update($requestData);
       return $this->success("Category updated correctly", $categoryUpdated);
     } catch (BaseException $ex) {
       return $this->fail($ex);
@@ -62,13 +63,13 @@ class CategoryController extends BaseController
    * requirements={"id"="\d+"},
    * methods={"DELETE"})
    */
-  public function delete(Request $request, CategoryManager $categoryManager): JsonResponse
+  public function delete(Request $request, CategoryRepository $repo): JsonResponse
   {
     try {
-      $category = $categoryManager->find($request->get('id'));
+      $category = $repo->find($request->get('id'));
       if (!$category) throw new NotFoundException("Category not found");
 
-      $categoryManager->delete($category);
+      $repo->delete($category);
       return $this->success("Category deleted correctly");
     } catch (BaseException $ex) {
       return $this->fail($ex);
