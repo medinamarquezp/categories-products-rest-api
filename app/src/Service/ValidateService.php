5123188eb3a7a50a7c48544c12d438aa8ca46970
Pedro Medina Márquez <medinamarquezp@gmail.com>
2020-12-15 04:29:49 +0100

    refactor: validator migrated to service

--- app/src/Service/ValidateService.php
+++ app/src/Service/ValidateService.php
@@ -4,37 +4,64 @@ namespace App\Service;
 
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-class ValidateService {
+class ValidateService
+{
 
   private $validator;
 
   public function __construct(ValidatorInterface $validator)
   {
-      $this->validator = $validator;
+    $this->validator = $validator;
   }
 
   public function validate($data)
   {
     $errors = $this->validator->validate($data);
-    if($errors) {
+    if ($errors) {
       return $this->printErrors($errors);
     }
     return null;
   }
 
-  private function printErrors($errors) {
+  public function validateCategory($request, $data)
+  {
+    $hasCategoryOnRequest = !empty($request->get("category"));
+    $categoryIsNull = empty($data->getCategory());
+    $errorCategory = $hasCategoryOnRequest && $categoryIsNull;
+
+    $errors = $this->validate($data);
+
+    if ($errors) {
+      if ($errorCategory) {
+        return $this->printCategoryError($errors);
+      }
+      return $errors;
+    } else {
+      if ($errorCategory) {
+        return $this->printCategoryError($errors);
+      }
+    }
+    return null;
+  }
+
+  private function printCategoryError($errors){
+    $errorCategoryMessage = "Category selected does not exists";
+    $errors["category"] = [];
+    array_push($errors["category"], $errorCategoryMessage);
+    return $errors;
+  }
+
+  private function printErrors($errors)
+  {
     $result = [];
     foreach ($errors as $error) {
       $property = $error->getPropertyPath();
       $errorMessage = $error->getMessage();
-      if(!array_key_exists($property, $result)){
+      if (!array_key_exists($property, $result)) {
         $result[$property] = [];
       }
       array_push($result[$property], $errorMessage);
-
     }
     return $result;
   }
-
-
 }
