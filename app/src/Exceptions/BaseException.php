03ab12bcc13c0997959ce486cb1d071ebc6d9589
Pedro Medina Márquez <medinamarquezp@gmail.com>
2020-12-15 01:49:40 +0100

    fix: added empty array as default value on errors parameter

--- app/src/Exceptions/BaseException.php
+++ app/src/Exceptions/BaseException.php
@@ -6,7 +6,7 @@ abstract class BaseException extends \RuntimeException implements \Throwable
 {
     private Array $errorList;
 
-    public function __construct($message = "It seems that something has gone wrong", int $statusCode = 500, Array $errorList)
+    public function __construct($message = "It seems that something has gone wrong", int $statusCode = 500, Array $errorList = [])
     {
         parent::__construct($message, $statusCode);
         $this->errorList = $errorList;
